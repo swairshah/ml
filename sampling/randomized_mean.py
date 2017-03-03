d9ae53f6bb3e5b33c59f04ab032e8b51e64e874a
swairshah <swairshah@gmail.com>
2017-03-03 15:30:22 -0600

    calculate vector means - updat randomized_mean.py

--- sampling/randomized_mean.py
+++ sampling/randomized_mean.py
@@ -6,30 +6,31 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression 
 from sklearn import datasets
 from sklearn.datasets import make_regression
+from numpy.linalg import norm
 
 
 def experiment():
-    m = 1000
+    m = 100
     n = 10
     r = 100
     x = np.random.uniform(0, 1000, size = m*n)
     X = np.reshape(x, (m,n))
-    prob = x/sum(x)
-    print x/prob
-    mean = np.mean(x)
-
+    normX = np.linalg.norm(X)
+    prob = np.linalg.norm(X, axis = 1)**2/normX**2
+    mean = np.mean(X, axis = 0)
+    
+    X_ = X/(m*prob[:,None])
     uni_means = []
     imp_means = []
-    print x_
     for i in range(100):
         idx1 = np.random.randint(0, m, size = r)
-        m1 = np.mean(x[idx1])
-        uni_means.append(m1)
+        m1 = np.mean(X[idx1,:], axis=0)
+        uni_means.append(norm(m1))
     
-        idx2 = np.random.choice(range(len(x)), size = r, p=prob )
-        m2 = np.mean(x_[idx2])
-        imp_means.append(m2)
-        print mean, m1, m2
+        idx2 = np.random.choice(range(m), size = r, p=prob/sum(prob) )
+        m2 = np.mean(X_[idx2,:],axis = 0)
+        imp_means.append(norm(m2))
+        #print norm(mean), norm(m1), norm(m2)
 
     plt.subplot(211)
     plt.hist(uni_means, color="blue", label = str(np.var(uni_means)))
@@ -37,9 +38,8 @@ def experiment():
     plt.subplot(212)
     plt.hist(imp_means, color="orange", label = str(np.var(imp_means)))
     plt.legend()
-    #plt.show()
-    return (np.var(uni_means) - np.var(imp_means))
-
+    plt.show()
+    #return (np.var(uni_means) - np.var(imp_means))
 """
 better = []
 for i in range(100):
@@ -50,4 +50,4 @@ better = np.array(better)
 print len(better[better > 0])
 print better
 """
-experiment()
+print experiment()
