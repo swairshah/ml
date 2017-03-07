25c9d9fb87db5b89136d417e596b7a1d6ab803e2
Swair Shah <swairshah@gmail.com>
2017-03-06 18:52:07 -0600

    fixed weights stuff

--- sampling/error_estimate.py
+++ sampling/error_estimate.py
@@ -4,18 +4,16 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import pylab
-from scipy import stats
-import pandas as pd
 from sklearn.linear_model import LinearRegression 
 from sklearn import datasets
 from sklearn.datasets import make_regression
 
-m = 1000
+np.random.seed(0)
+m = 10000
 n = 50
-I = 10
+I = 100
 r = 10
-weighted = False
+weighted = True
 
 X, y, coef = make_regression(n_samples = m, n_features = n, noise = 10, coef = True)
 
@@ -26,7 +24,6 @@ y_pred = lm.predict(X)
 e = (y - y_pred)**2
 w = e/np.sum(e)
 err_base = np.mean(e)
-
 
 # uniform sampling
 unif_errors = []
@@ -43,25 +40,35 @@ for i in range(I):
     idx = np.random.choice(range(len(X)), r, p = w)
     w1 = w[idx]
     if weighted:
-        X1 = X[idx,:]/(m*w1[:,None])
-        y1 = y[idx]/(m*w1[:,None])
+        X1 = X[idx,:]/(m*w1[:,None])**0.5
+        y1 = y[idx]/(m*w1)**0.5
     else:
         X1 = X[idx,:]
         y1 = y[idx]
+    e = np.mean((lm.predict(X1) - y1)**2)
+
+    # The following should be equlivalent to 
+    # dividing x and y by sqrt(m*w1), since we are 
+    # dividing the final error by m*w1.
+    # doesn't give the same answer.
+    
     #X1 = X[idx,:]
     #y1 = y[idx]
-    e = np.mean((lm.predict(X1) - y1)**2)
+    #e = np.mean((lm.predict(X1) - y1)**2)
+    #if weighted:
+    #    e = e/(m*w1)
     imp_errors.append(e)
 
-
-print imp_errors, unif_errors
 print "base error: %.2f" % err_base
-print "variance importance %.2f" % np.var(imp_errors)
-print "variance uniform %.2f" % np.var(unif_errors)
+print "imp err", np.mean(imp_errors), "uni err", np.mean(unif_errors)
+#print "variance importance %.2f" % np.var(imp_errors)
+#print "variance uniform %.2f" % np.var(unif_errors)
+print "variance importance %.2f" % np.mean((imp_errors - err_base)**2)
+print "variance uniform %.2f" % np.mean((unif_errors - err_base)**2)
 
-plt.subplot(211)
-plt.hist(unif_errors, color="blue")
-plt.subplot(212)
-plt.hist(imp_errors, color="orange")
+#plt.subplot(211)
+#plt.hist(unif_errors, color="blue")
+#plt.subplot(212)
+#plt.hist(imp_errors, color="orange")
 #plt.show()
 
--- sampling/sampled_reg.py
+++ sampling/sampled_reg.py
@@ -1,21 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import pylab
-from scipy import stats
-import pandas as pd
 from sklearn.linear_model import LinearRegression 
+from sklearn.datasets import make_regression
 from sklearn import datasets
-
-def regression(X, y):
-    pseudoinv = np.linalg.pinv(np.dot(X.T,X))
-    a = np.dot(np.dot(pseudoinv,X.T),y)
-    return a
-
-def reg(x, y):
-    slope, intercept, r_value, p_value, std_err = stats.linregress(x,y)
-    #line = slope*x+intercept
-    return np.array(slope, intercept)
-
 
 def sampled_regression(sampling, X, y, I, r):
     """
@@ -24,53 +11,66 @@ def sampled_regression(sampling, X, y, I, r):
         X, y
         I: Iterations
         r: row sample size
-        
-        call example:
-        sampled_regression(sampling='importance',X=X, y=y, I=10, r=10)
+    call example:
+        sampled_regression('importance',X, y, I, r)
     """
-
+    
+    m = len(X)
     sample_errs = []
     min_err = float('inf')
     best_model = None
     w = np.copy(np.ones(len(X)))
-    w = w/sum(w)
-    
+    w = w/sum(w) 
     for i in range(I):
         if sampling.lower() == 'importance':
             idx = np.random.choice(range(len(X)), r, p = w)
+            w1 = w[idx]
+            X1 = X[idx,:]/(m*w1[:,None])**0.5
+            y1 = y[idx]/(m*w1)**0.5
         else: # uniform
             idx = np.random.randint(len(X),size=r)
+            X1 = X[idx,:]
+            y1 = y[idx]
 
-        X1 = X[idx,:]/w[idx]
-        y1 = y[idx]/w[idx]
-        a1 = regression(X1,y1)
-
-        e = (y - np.dot(X, a1))**2
+        lm = LinearRegression()
+        lm.fit(X1, y1)
+        e = (y - lm.predict(X))**2
         error = np.mean(e)
         sample_errs.append(error)
 
         if error < min_err:
-            best_model = a1
             min_err = error
             w = e/np.sum(e)
 
     return sample_errs
 
 if __name__ == "__main__":
-    diabetes = datasets.load_diabetes()
-    X = diabetes.data
-    y = diabetes.target
+    m = 10000
+    n = 5
+    X, y, coef = make_regression(n_samples = m, n_features = n, noise = 10, coef = True)
 
     lm = LinearRegression()
     lm.fit(X,y)
     base_err = np.mean((lm.predict(X) - y)**2)
     print "base err %.2f" % base_err
+    imp_errs = []
+    uni_errs = []
+    for i in range(10,100):
+        imp = sampled_regression('importance', X, y, 10, 50)
+        uni = sampled_regression('uniform', X, y, 10, 50)
+        imp_errs.append(np.mean(imp))
+        uni_errs.append(np.mean(uni))
 
-    imp = sampled_regression('importance', X, y, 10, 10)
-    uni = sampled_regression('uniform', X, y, 10, 10)
-    #print imp
-    #print uni
-    plt.plot(imp)
-    plt.plot(uni, c = 'orange')
-    plt.legend(['importance','uniform'])
+    plt.plot(range(10,100), imp_errs, label="imp")
+    plt.plot(range(10,100), uni_errs, label="uni")
+    plt.legend()
     plt.show()
+    #print "importance err %.2f, var %.2f" % (np.mean(imp), np.mean((imp - base_err)**2))
+    #print "uniform err %.2f, var %.2f" % (np.mean(uni), np.mean((uni - base_err)**2))
+
+    #plt.subplot(211)
+    #plt.hist(uni, color="blue")
+    #plt.subplot(212)
+    #plt.hist(imp, color="orange")
+    #plt.show()
+
